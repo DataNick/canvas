5e983653729e0b77051ba5aa154b35cb87a637ab
DataNick <DataNick@users.noreply.github.com>
2015-09-14 22:51:52 -0400

    Added and set up devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
@@ -11,11 +13,11 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post=Post.new(post_params)
+		@post=current_user.posts.build(post_params)
 
 		if @post.save
 			redirect_to @post
@@ -34,7 +36,7 @@ class PostsController < ApplicationController
 
 	def destroy
 		@post.destroy
-		redirect_to root_path
+		redirect_to root_path	
 	end
 
 
---
+++ db/migrate/20150915024746_add_user_id_post.rb
@@ -0,0 +1,4 @@
+class AddUserIdPost < ActiveRecord::Migration
+  def change
+  end
+end
