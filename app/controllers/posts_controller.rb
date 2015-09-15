16fa1cdc36389c169ada8d8764fb3a2e79c7918a
DataNick <DataNick@users.noreply.github.com>
2015-09-14 22:24:23 -0400

    Added actions and views to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,34 @@ class PostsController < ApplicationController
 	def edit
 	end
 
+	def show
+	end
+
+	def new
+		@post = Post.new
+	end
+
 	def create
-		@post=Post.build(post_params)
+		@post=Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@post.destroy
+		redirect_to root_path
 	end
 
 
